e80eeacd26974258cd3d676b83328f472d8af721
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-05 22:28:14 +0000

    implement adapter

---
+++ src/main/java/com/example/FrenchLocalizedMessageAdapter.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class FrenchLocalizedMessageAdapter extends FrenchLocalizedMessage
+    implements LocalizedMessage {
+
+  @Override
+  public void sayHello() {
+    sayBonjour();
+  }
+
+}
