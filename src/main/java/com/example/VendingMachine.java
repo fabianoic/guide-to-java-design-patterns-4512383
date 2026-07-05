9c7db96fb0509f4bc070b85240f131a2b3c91d19
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-05 13:31:30 +0000

    implement the factory method

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,10 +1,24 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.function.Supplier;
+
 public class VendingMachine {
 
-    Snack getSnack(String snackType) {
-        return null;
+    private static final HashMap<String, Supplier<Snack>> snackMap = new HashMap<>();
+
+    static {
+        snackMap.put("Drink", Drink::new);
+        snackMap.put("Chips", Chips::new);
+        snackMap.put("ChocolateBar", ChocolateBar::new);
     }
 
+    Snack getSnack(String snackType) {
+        if (snackMap.get(snackType) != null) {
+            return snackMap.get(snackType).get();
+        } else {
+            throw new IllegalArgumentException("Invalid snacktype");
+        }
+    }
 
 }
