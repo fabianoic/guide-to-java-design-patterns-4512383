0d7b3ede5191a42cda4299d122a632da94a86792
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-07 20:58:52 +0000

    implement bridge pattern

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,11 +3,13 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        var circle = new Circle();
+        var circle = new Circle(new Blue());
         circle.getType();
+        circle.getColor().get();
 
-        var square = new Square();
+        var square = new Square(new Red());
         square.getType();
+        square.getColor().get();
     }
 
 }
