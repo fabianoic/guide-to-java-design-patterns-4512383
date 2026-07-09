75af0960647ebabb47185053582562fdbe51eb3d
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-09 21:54:19 +0000

    implement template pattern

--- src/main/java/com/example/Recipe.java
+++ src/main/java/com/example/Recipe.java
@@ -5,14 +5,10 @@ public class Recipe {
     public static void main(String[] args) {
 
         var vegetarianPizza = new VegetarianPizza();
-        vegetarianPizza.makeBase();
-        vegetarianPizza.addToppings();
-        vegetarianPizza.cook();
+        vegetarianPizza.preparePizza();
 
         var meatFeastPizza = new MeatFeastPizza();
-        meatFeastPizza.makeBase();
-        meatFeastPizza.addToppings();
-        meatFeastPizza.cook();
+        meatFeastPizza.preparePizza();
 
     }
 
--- src/main/java/com/example/VegetarianPizza.java
+++ src/main/java/com/example/VegetarianPizza.java
@@ -1,22 +1,9 @@
 package com.example;
 
-public class VegetarianPizza {
+public class VegetarianPizza extends Pizza {
 
-
-    public void makeBase() {
-        System.out.println("Mix flour, yeast and salt.");
-        System.out.println("Roll out the dough.");
-    }
-
+    @Override
     public void addToppings() {
         System.out.println("Add cheese, tomato, mushrooms, peppers and olives" );
     }
-
-    public void cook() {
-        System.out.println("Cook in the oven for 20 minutes");
-    }
-
-
-
-
 }
