29d138c3835729021d114c2ab28b4bec46b73be9
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-09 20:24:34 +0000

    implement memento pattern

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -3,9 +3,11 @@ package com.example;
 public class Item {
 
     private int price;
+    private ItemState state;
 
     public Item(int price) {
         this.price = price;
+        this.state = new ItemState(price);
     }
 
     public void setPrice(int price) {
@@ -14,6 +16,14 @@ public class Item {
 
     public int getPrice() {
         return price;
+    }
+
+    public void save() {
+        this.state.setState(this.price);
+    }
+
+    public void undo() {
+        this.price = state.getState();
     }
 
 }
