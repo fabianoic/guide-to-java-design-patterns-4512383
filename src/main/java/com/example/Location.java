001edaacb2254cabe5cfc0d7fccb0ead0f3eaf48
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-09 20:43:49 +0000

    implement observer pattern

--- src/main/java/com/example/Location.java
+++ src/main/java/com/example/Location.java
@@ -1,11 +1,20 @@
 package com.example;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class Location {
 
     private String weatherEvent;
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public void setWeatherEvent(String weatherEvent) {
+        support.firePropertyChange("weatherEvent", this.weatherEvent, weatherEvent);
         this.weatherEvent = weatherEvent;
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
     }
 
 }
