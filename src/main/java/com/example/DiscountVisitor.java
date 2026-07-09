cae62a38d34a67554b824f1b8c0ae85ebf9a22aa
fabianoic <49404719+fabianoic@users.noreply.github.com>
2026-07-09 22:34:11 +0000

    implement visitor pattern

---
+++ src/main/java/com/example/DiscountVisitor.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class DiscountVisitor implements Visitor {
+
+  @Override
+  public void visit(AdultTicket adultTicket) {
+    adultTicket.setPrice(5);
+  }
+
+  @Override
+  public void visit(GroupTicket groupTicket) {
+    groupTicket.getElements().forEach(e -> e.accept(this));
+  }
+
+}
---
+++ src/main/java/com/example/Visitor.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public interface Visitor {
+
+  void visit(AdultTicket adultTicket);
+
+  void visit(GroupTicket groupTicket);
+
+}
